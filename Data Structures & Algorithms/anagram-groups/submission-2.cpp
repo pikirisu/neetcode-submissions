9f175fa36142aefc8bc61ffaba0fb42c66b9ce68
Akshat <149987358+pikirisu@users.noreply.github.com>
2026-05-27 10:34:03 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mapper;
+        for(const string &s : strs){
+
+            vector<int> freq(26,0);
+
+            for(char c:s){
+                freq[c-'a']++;
+            }
+
+            string key = "";
+
+            for(int count:freq){
+                key+=to_string(count)+"#";
+            }
+
+            mapper[key].push_back(s);
+        }
+
+        for(auto it:mapper){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
