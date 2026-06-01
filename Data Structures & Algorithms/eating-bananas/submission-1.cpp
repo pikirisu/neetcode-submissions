0720e97b667a85a1166ef1b7a111ac5495c95ea5
Akshat <149987358+pikirisu@users.noreply.github.com>
2026-06-01 11:34:36 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool helper(vector<int>& piles, int mid, int h){
+        int curr = 0;
+        int n = piles.size();
+        for(int i=0;i<n;i++){
+            curr+=((piles[i]+mid-1)/mid);
+        }
+        return (curr<=h);
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int maxi = INT_MIN;
+
+        for(int i=0;i<n;i++){
+            maxi = max(maxi,piles[i]);
+        }
+
+        int l = 1;
+        int r = maxi;
+        int ans = INT_MAX;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            cout << "mid = " << mid << " helper = " << helper(piles, mid, h)<< endl;
+            if(helper(piles,mid,h)){
+                ans = mid;
+                r=mid-1;
+            }
+            else l = mid+1;
+        }
+        return ans;
+    }
+};
