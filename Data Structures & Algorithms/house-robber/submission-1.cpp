56b878e0b8d5a16bfaf1891618f8dc7ed63657e9
Akshat <149987358+pikirisu@users.noreply.github.com>
2026-06-17 13:28:45 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int index){
+        // Base Case
+        if(index>=nums.size())
+        return 0;
+
+        int include =  nums[index]+solve(nums,index+2);
+
+        int exclude = 0 + solve(nums,index+1);
+
+        return max(include,exclude);
+    }
+
+        int solveMem(vector<int>& nums,int index,vector<int>&dp){
+        // Base Case
+        int n = nums.size();
+        if(index>=n)
+        return 0;
+
+        if(dp[index]!=-1)
+        return dp[index];
+
+        int include =  nums[index]+solveMem(nums,index+2,dp);
+
+        int exclude = 0 + solveMem(nums,index+1,dp);
+        dp[index] =  max(include,exclude);
+        return dp[index];
+    }
+
+    int rob(vector<int>& nums) {
+        // return solve(nums,0);
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return solveMem(nums,0,dp);
+    }
+};
