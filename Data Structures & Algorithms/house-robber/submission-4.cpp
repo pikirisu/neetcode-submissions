293a9b31492ab26a1b52fae58773a0d5f3f70da7
Akshat <149987358+pikirisu@users.noreply.github.com>
2026-06-17 15:03:18 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int index){
+        // Base Case
+        if(index>=nums.size())
+        return 0;
+
+        int include =  nums[index]+solve(nums,index+2);
+
+        int exclude = 0 + solve(nums,index+1);
+
+        return max(include,exclude);
+    }
+
+    int solveMem(vector<int>& nums,int index,vector<int>&dp){
+        // Base Case
+        int n = nums.size();
+        if(index>=n)
+        return 0;
+
+        if(dp[index]!=-1)
+        return dp[index];
+
+        int include =  nums[index]+solveMem(nums,index+2,dp);
+
+        int exclude = 0 + solveMem(nums,index+1,dp);
+        dp[index] =  max(include,exclude);
+        return dp[index];
+    }
+
+    int solveTab(vector<int>& nums){
+        // Base Case
+        int n = nums.size();
+        vector<int> dp(n+2,0);
+        for(int index=n-1; index>=0; index--){
+            
+            int include =  nums[index]+dp[index+2];
+
+            int exclude = 0 + dp[index+1];
+
+            dp[index] =  max(include,exclude);
+        }
+
+        return max(dp[0],dp[1]);
+    }
+
+    int solveTabSpace(vector<int>& nums){
+        // Base Case
+        int n = nums.size();
+        int next1 = 0;
+        int next2 = 0;
+        int curr = 0;
+        for(int index=n-1; index>=0; index--){
+            
+            int include =  nums[index]+next2;
+
+            int exclude = 0 + next1;
+
+            int curr = max(include, exclude);
+
+            // Shifting
+            next2 = next1;
+            next1 = curr;
+        }
+
+        return max(next1,next2);
+    }
+
+    int rob(vector<int>& nums) {
+        // return solve(nums,0);
+        int n = nums.size();
+        // vector<int> dp(n+1,-1);
+        // return solveMem(nums,0,dp);
+        return solveTabSpace(nums);
+    }
+};
