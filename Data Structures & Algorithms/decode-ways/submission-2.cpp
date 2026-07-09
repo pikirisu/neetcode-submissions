606c3af1bf289b5229f8ca7354a2b1544915bc0c
Akshat <149987358+pikirisu@users.noreply.github.com>
2026-07-09 16:19:37 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int solve(string &s, int i){
+        // Base Case
+        if(i==s.length())
+        return 1;
+
+        if(s[i]=='0') return 0;
+
+        int ans = solve(s,i+1);
+
+        if (i + 1 < s.length()) {
+            int num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            if (num >= 10 && num <= 26)
+                ans += solve(s, i + 2);
+            }
+
+        return ans;
+    }
+
+    int solveMem(string &s, int i,vector<int>&dp){
+        // Base Case
+        if(i==s.length())
+        return 1;
+
+        if(s[i]=='0') return 0;
+
+        if(dp[i]!=-1)
+        return dp[i];
+
+        dp[i] = solveMem(s,i+1,dp);
+
+        if (i + 1 < s.length()) {
+            int num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            if (num >= 10 && num <= 26)
+                dp[i] += solveMem(s, i + 2,dp);
+            }
+
+        return dp[i];
+    }
+
+    int solveTab(string &s){
+        int n = s.length();
+        // Automatically handles base case
+        vector<int>dp(n+2,0);
+        dp[n]=1;
+
+        for(int i=n-1; i>=0; i--){
+            if(s[i]=='0'){
+            dp[i] = 0;
+            continue;
+            }
+        dp[i] = dp[i+1];
+
+        if (i + 1 < s.length()) {
+            int num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            if (num >= 10 && num <= 26)
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+    int numDecodings(string s) {
+        // int ans = solve(s,0);
+        // return ans;
+        // int n = s.length();
+        // vector<int>dp(n,-1);
+        // int ans = solveMem(s,0,dp);
+        int ans = solveTab(s);
+        return ans;
+    }
+};
